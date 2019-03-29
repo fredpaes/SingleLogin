fe76f0eb997b5e870e1e57678040cd7afa76f738
Fred <fred.paucarespinoza@gmail.com>
2019-03-29 18:12:34 -0500

    adding defs

--- app/usuario/views.py
+++ app/usuario/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def log_in(request):
     return render(request, 'usuario/login.html')
+
+def log_out(request):
+    pass
+
+def cambio_password(request):
+    pass
