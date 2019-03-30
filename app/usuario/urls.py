c82ced88b8d86835e7cc13880c70849a4df763a3
Fred <fred.paucarespinoza@gmail.com>
2019-03-30 10:47:19 -0500

    funcion login adicionada

--- app/usuario/urls.py
+++ app/usuario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import log_in
+from .views import log_in, home
 
 urlpatterns = [
     path('login/', log_in, name='login'),
+    path('index/', home, name='index'),
 ]
--- app/usuario/views.py
+++ app/usuario/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def log_in(request):
-    return render(request, 'usuario/login.html')
+    if request.method is 'POST':
+        # autenticación
+        u_name = request.POST.get('username')
+        u_pass = request.POST.get('password')
+        user = authenticate(request, username=u_name, password=u_pass)
+        if user is None:
+            # el usuario esta mal
+            pass
+        elif user.is_active:
+            # login
+            login(request, user)
+            return redirect('index')
+        else:
+            # el usuario no esta activo
+            pass
+    else:
+        return render(request, 'usuario/login.html')
 
 def log_out(request):
     pass
 
 def cambio_password(request):
     pass
+
+def home(request):
+    return render(request, 'usuario/index.html')
